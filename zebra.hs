34aa0b3fc756af15f45b6ae4d522a50e0de72a56
Abhi Agarwal <me@abhiagarwal.com>
2016-01-10 12:39:41 +0700

    Adding tests

--- zebra.hs
+++ zebra.hs
@@ -91,28 +91,68 @@ showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tai
 instance Show LispVal where show = showVal
 
 -- Evaluator: adding basic primitives
-numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
-numericBinop op [] = throwError $ NumArgs 2 []
-numericBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
-numericBinop op params = mapM unpackNum params >>= return . Number . foldl1 op
-
-unpackNum :: LispVal -> ThrowsError Integer
-unpackNum (Number n) = return n
-unpackNum (String n) = let parsed = reads n in
-    if null parsed
-        then throwError $ TypeMismatch "number" $ String n
-        else return $ fst $ parsed !! 0
-unpackNum (List [n]) = unpackNum n
-unpackNum notNum = throwError $ TypeMismatch "number" notNum
 
 primitives :: [(String, [LispVal] -> ThrowsError LispVal)]
 primitives = [("+", numericBinop (+)),
     ("-", numericBinop (-)),
     ("*", numericBinop (*)),
     ("/", numericBinop div),
+    ("=", numBoolBinop (==)),
+    ("<", numBoolBinop (<)),
+    (">", numBoolBinop (>)),
+    ("/=", numBoolBinop (/=)),
+    (">=", numBoolBinop (>=)),
+    ("<=", numBoolBinop (<=)),
+    ("&&", boolBoolBinop (&&)),
+    ("||", boolBoolBinop (||)),
+    ("string=?", strBoolBinop (==)),
+    ("string<?", strBoolBinop (<)),
+    ("string>?", strBoolBinop (>)),
+    ("string<=?", strBoolBinop (<=)),
+    ("string>=?", strBoolBinop (>=)),
     ("mod", numericBinop mod),
     ("quotient", numericBinop quot),
     ("remainder", numericBinop rem)]
+
+numericBinop :: (Integer -> Integer -> Integer) 
+             -> [LispVal] 
+             -> ThrowsError LispVal
+numericBinop op single@[_] = throwError $ NumArgs 2 single
+numericBinop op params = mapM unpackNum params >>= return . Number . foldl1 op
+
+numBoolBinop = boolBinop unpackNum
+strBoolBinop = boolBinop unpackStr
+boolBoolBinop = boolBinop unpackBool
+
+unaryOp :: (LispVal -> LispVal) 
+        -> [LispVal] 
+        -> ThrowsError LispVal
+unaryOp func [arg] = return $ func arg
+
+boolBinop :: (LispVal -> ThrowsError a) 
+          -> (a -> a -> Bool) 
+          -> [LispVal] 
+          -> ThrowsError LispVal
+boolBinop unpacker op [x,y] = do
+    left <- unpacker x
+    right <- unpacker y
+    return $ Bool $ left `op` right
+boolBinop _ _ args = throwError $ NumArgs 2 args
+
+unpackNum :: LispVal -> ThrowsError Integer
+unpackNum (Number n) = return n
+unpackNum (List [n]) = unpackNum n
+unpackNum notNum = throwError $ TypeMismatch "number" notNum
+
+unpackStr :: LispVal -> ThrowsError String
+unpackStr (String s) = return s
+unpackStr (Number s) = return $ show s
+unpackStr (Bool s) = return $ show s
+unpackStr notStr = throwError $ TypeMismatch "string" notStr
+
+unpackBool :: LispVal -> ThrowsError Bool
+unpackBool (Bool b) = return b
+unpackBool notBool = throwError $ TypeMismatch "boolean" notBool
 
 -- Error checking
 -- Defining the types of possible errors
@@ -165,6 +205,11 @@ eval val@(String _) = return val
 eval val@(Number _) = return val
 eval val@(Bool _) = return val
 eval (List [Atom "quote", val]) = return val
+eval (List [Atom "if", pred, conseq, alt]) = 
+     do result <- eval pred
+        case result of
+             Bool False -> eval alt
+             otherwise  -> eval conseq
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
